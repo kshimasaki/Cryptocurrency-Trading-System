454e1c7289af8b8813f18d33da7254e9b610a21d
Srivathsan Subramanian <Sri@Srivathsans-MacBook-Pro.local>
2017-08-06 18:43:34 -0700

    changed search query to just ticker and full name

--- botstrategy.py
+++ botstrategy.py
@@ -51,7 +51,7 @@ class BotStrategy(object):
 
 		if (len(openTrades) < self.numSimulTrades):
 			if (self.btc_historical_total < 50000):
-				bitcoin_query = 'BTC AND Bitcoin AND Price'
+				bitcoin_query = 'BTC AND Bitcoin'
 
 				btc_historical_tweets, self.btc_sinceid = tweets.get_tweets(30, self.btc_sinceID, bitcoin_query)
 				btc_total_score, btc_positive, btc_negative, btc_total = tweets.classify(btc_historical_tweets)
@@ -62,7 +62,7 @@ class BotStrategy(object):
 
 				self.btc_historical_percent = (self.btc_historical_positive / self.btc_historical_total) * 100
 
-				ethereum_query = 'Ethereum AND ETH AND Price'
+				ethereum_query = 'Ethereum AND ETH'
 				#eth_tweets, sinceid_recent = tweets.get_tweets(3,0,ethereum_query)
 
 				#eth_total_score2, eth_positive2, eth_negative2, eth_total2 = tweets.classify(eth_tweets)
@@ -119,7 +119,7 @@ class BotStrategy(object):
 
 			if (self.type_of_trade == 'BTC'):
 
-				bitcoin_query = 'BTC AND Bitcoin AND Price'
+				bitcoin_query = 'BTC AND Bitcoin'
 				btc_tweets, sinceid_recent = tweets.get_tweets(3,0,bitcoin_query)
 
 				btc_total_score2, btc_positive2, btc_negative2, btc_total2 = tweets.classify(btc_tweets)
@@ -143,7 +143,7 @@ class BotStrategy(object):
 					time.sleep(60)
 
 			if (self.type_of_trade == 'ETH'):
-				ethereum_query = 'Ethereum AND ETH AND Price'
+				ethereum_query = 'Ethereum AND ETH'
 				eth_tweets, sinceid_recent = tweets.get_tweets(3,0,ethereum_query)
 
 				eth_total_score2, eth_positive2, eth_negative2, eth_total2 = tweets.classify(eth_tweets)
